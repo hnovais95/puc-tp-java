f0e12bf7b4a625ee45b6b90b09823b440a8ab89f
Heitor Novais <hnovais95@gmail.com>
2023-03-19 18:02:20 -0300

    feat: implement message queue and simplify code

---
+++ purchase-ms/src/main/kotlin/br/puc/tp_final/purchase/model/PurchaseException.kt
@@ -0,0 +1,4 @@
+package br.puc.tp_final.purchase.model
+import java.util.*
+
+class PurchaseException(message: String) : Exception(message)
--- purchase-ms/src/main/kotlin/br/puc/tp_final/purchase/model/Response.kt
+++ purchase-ms/src/main/kotlin/br/puc/tp_final/purchase/model/Response.kt
@@ -1,18 +1,8 @@
 package br.puc.tp_final.purchase.model
-
 import java.util.*
+
 data class Response (
     val statusCode: Int,
     val friendlyMessage: String,
-    val uuid: UUID
-) {
-    class Builder {
-        var statusCode: Int = 0
-        var friendlyMessage: String = ""
-        var uuid: Int = 0
-
-        fun build(): Response {
-            return Response(statusCode, friendlyMessage, UUID.randomUUID())
-        }
-    }
-}
+    val uuid: UUID? = null
+)
